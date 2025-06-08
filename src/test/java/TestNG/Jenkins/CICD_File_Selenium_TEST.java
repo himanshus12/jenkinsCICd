657cabe16c99fa9135a001dc6ac1ca0914fb0e8a
dell <himanshusharma051999@gmail.com>
2025-06-09 00:30:26 +0530

    Initial commit for the jenkins cicd testcase run

---
+++ src/test/java/TestNG/Jenkins/CICD_File_Selenium_TEST.java
@@ -0,0 +1,31 @@
+package TestNG.Jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class CICD_File_Selenium_TEST {
+	WebDriver driver;
+	
+	@Test
+	public void setup() {
+		
+		 driver = new EdgeDriver();
+		driver.get("https://www.google.com/");
+		driver.quit();
+	}
+	
+	@Test
+	public void test() throws InterruptedException {
+		Thread.sleep(5000);
+	}
+	
+	
+	@Test
+	public void print() {
+System.out.println("QA");
+
+	}
+	
+	
+}
